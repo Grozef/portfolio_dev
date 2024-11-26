5e9d4f00e6bb28671fe7b3875d235fd4469107ac
Francois <francois.lisowski@lab.fr>
2024-11-26 14:44:50 +0100

    npm tailwind ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    './templates/**/*.html.twig',
-    './assets/**/*.js',
+    './templates/**/*.html.twig', // Chemin vers vos fichiers Twig
+    './assets/**/*.js',          // Si vous utilisez des fichiers JavaScript
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
